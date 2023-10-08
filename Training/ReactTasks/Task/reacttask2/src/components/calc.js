f0c851b74e1216d2cd67ff62a06dc18825168200
M4dar4 <arush.sdl.shrivastava@gmail.com>
2023-10-08 19:14:43 +0530

    react task 2

--- Training/ReactTasks/Task/reacttask2/src/components/calc.js
+++ Training/ReactTasks/Task/reacttask2/src/components/calc.js
@@ -6,13 +6,37 @@ import Numpad from './numPad'
 import React, { useState } from 'react';
 
 function Calculator() {
-    const [isSun, setIsSun] = useState(true);
-    // const [cardClass, setCardClass] = useState('card-light');
+    const [isSun, setIsSun] = useState(false);
+    const math = require('mathjs');
   
     const toggleTheme = () => {
         setIsSun((prevIsSun) => !prevIsSun)
     }
     const cardClass = isSun ? 'card-light' : 'card-dark';
+    const [inputVal, changeIn] = useState('')
+    const [calVal, calculate] = useState('')
+
+    const [history, setHistory] = useState([]);
+
+    const handleInput = (event)=>{
+      if(event === 'backspace'){
+        changeIn((prevInputValue) => prevInputValue.slice(0, -1));
+      }
+      else if(event === 'calculate'){
+        calculate(math.evaluate(inputVal))
+        setHistory((prevHistory) => {
+          const newHistory = [...prevHistory, inputVal];
+          if (newHistory.length > 3) {
+            newHistory.shift();
+          }
+          return newHistory;
+        });
+      }
+      else{
+        changeIn((prevInputValue) => prevInputValue + event)
+      }
+    }
+
   
     return (
       <div className={`card rounded-5 w-25 h-75  d-flex flex-column ${cardClass}`}>
@@ -20,8 +44,18 @@ function Calculator() {
           <Circle />
           {isSun ? <Sun onClick={toggleTheme} /> : <Moon onClick={toggleTheme} />}
         </div>
+        <div className="text-end pe-4 h-50 d-flex flex-column">
+          {history.map((item, index) => (
+            <p key={index}>
+              {item}
+            </p>
+          ))}
+          <p className="mt-auto h5">{inputVal}</p>
+          <p className="mt-auto h3">{calVal}</p>
+        </div>
+
         <div className='d-flex justify-content-center mt-auto h-50'>
-            <Numpad isSun={isSun}/>
+            <Numpad isSun={isSun} onButtonClick={handleInput}/>
         </div>
       </div>
     );
--- Training/ReactTasks/Task/reacttask2/src/components/numPad.js
+++ Training/ReactTasks/Task/reacttask2/src/components/numPad.js
@@ -1,32 +1,32 @@
 import { Backspace } from "./icons";
-function Numpad({isSun}){
+function Numpad({isSun, onButtonClick }){
     const keyPadClass = isSun ? 'keyPad-light' : 'keyPad-dark';
     return (
         <div className={`text-center rounded-bottom-5 row w-100 ${keyPadClass}`}>
             <div className="col d-flex flex-column justify-content-between mt-4">
-                <p>7</p>
-                <p>4</p>
-                <p>1</p>
-                <p>0</p>
+                <p onClick={() => onButtonClick('7')}>7</p>
+                <p onClick={() => onButtonClick('4')}>4</p>
+                <p onClick={() => onButtonClick('1')}>1</p>
+                <p onClick={() => onButtonClick('0')}>0</p>
             </div>
             <div className="col d-flex flex-column justify-content-between mt-4">
-                <p>8</p>
-                <p>5</p>
-                <p>2</p>
-                <p>.</p>
+                <p onClick={() => onButtonClick('8')}>8</p>
+                <p onClick={() => onButtonClick('5')}>5</p>
+                <p onClick={() => onButtonClick('2')}>2</p>
+                <p onClick={() => onButtonClick('.')}>.</p>
             </div>
             <div className="col d-flex flex-column justify-content-between mt-4">
-                <p>9</p>
-                <p>6</p>
-                <p>3</p>
-                <p>=</p>
+                <p onClick={() => onButtonClick('9')}>9</p>
+                <p onClick={() => onButtonClick('6')}>6</p>
+                <p onClick={() => onButtonClick('3')}>3</p>
+                <p onClick={() => onButtonClick('calculate')}>=</p>
             </div>
             <div className="col d-flex flex-column justify-content-between mt-3">
-                <p><Backspace toggle={isSun}/></p>
-                <p>÷</p>
-                <p>x</p>
-                <p>-</p>
-                <p>+</p>
+                <p onClick={() => onButtonClick('backspace')}><Backspace toggle={isSun}/></p>
+                <p onClick={() => onButtonClick('/')}>÷</p>
+                <p onClick={() => onButtonClick('*')}>x</p>
+                <p onClick={() => onButtonClick('-')}>-</p>
+                <p onClick={() => onButtonClick('+')}>+</p>
             </div>
         </div>
     )
