52697a11e8e6032e3a57eca705d05e79b2ff7f3f
M4dar4 <arush.sdl.shrivastava@gmail.com>
2023-10-11 11:00:52 +0530

    navbar sidebar left

--- Training/ReactTasks/Task/reacttask6/src/components/navBar.js
+++ Training/ReactTasks/Task/reacttask6/src/components/navBar.js
@@ -1,18 +1,38 @@
 import './customCSS/navbar.css'
 import { DarkModeToggle } from '@anatoliygatt/dark-mode-toggle';
-import Home from './homeBody';
+import React, { useState, useEffect } from 'react';
+import ListIcon from './customIcon/list';
 
 export default function NavBar({mode, setMode}){
-    
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const [sideBar, openSideBar] = useState(false);
+    const menuOpener = ()=>{
+        console.log(sideBar)
+        openSideBar(!sideBar)
+    }
+    useEffect(() => {
+        const handleResize = () => {
+          setWindowWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+      const navSize = windowWidth <= 1200 ? `container-fluid justify-content-between` :`mt-3 rounded-pill w-75`
+      const atagSize = windowWidth <= 1200 ? `container-fluid` :``
     return(
         <div>
-            <div className='d-flex justify-content-center heightInc'>
-                <nav className={`navbar navbar-expand-lg navbar_${mode} rounded-pill w-75 mt-3`}>
-                    <div className='collapse navbar-collapse row justify-content-between'>
-                        <div className="ms-4 d-flex justify-content-between col-6">
-                            <a className={`nav-link active ${mode}`} href={() => {}}>
-                                Arush Shrivastava
-                            </a>
+            <div className='d-flex justify-content-center'>
+                <nav className={`navbar navbar-expand navbar_${mode} ${navSize}`}>
+                    <a className={`ms-4 ${atagSize} nav-link active ${mode}`} href={() => {}}>
+                        Arush Shrivastava
+                    </a>
+                    <div className='d-xl-none container-fluid justify-content-end align-items-center'>
+                        <ListIcon onClick={menuOpener} />
+                    </div>
+                    <div className='ms-5 collapse navbar-collapse row justify-content-between'>
+                        <div className="d-flex justify-content-between col-5 d-none d-xl-flex">
                             <div className='nav-item dropdown'>
                                 <a className={`nav-link ${mode} dropdown-toggle`} role="button" id="Dropdown" data-bs-toggle="dropdown" aria-expanded="false" href={() => {}}>Projects</a>
                                 <div className={`dropdown-menu drop_${mode}`} aria-labelledby="Dropdown">
@@ -28,7 +48,7 @@ export default function NavBar({mode, setMode}){
                                 Skills
                             </a>
                         </div>
-                        <div className="d-flex justify-content-between col-3 me-5">
+                        <div className="d-flex justify-content-between col-4 me-4 d-none d-xl-flex">
                             <a className={`nav-link ${mode}`} href={() => {}}>
                                 Social
                             </a>
@@ -42,7 +62,7 @@ export default function NavBar({mode, setMode}){
                                 inactiveTrackColorOnActive="#cbd5e1"
                                 inactiveThumbColor='#FFBF00'
                                 activeTrackColor="#CF9FFF"
-                                activeTrackColorOnHover="#BF40BF"
+                                activeTrackColorOnHover="#df4ddf"
                                 activeThumbColor='Black'
                                 onChange={(mode) => {
                                     setMode(mode);
